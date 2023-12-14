9ccf90c137a403cea389d8f7c9a25b7b7ba67b9b
Arshiya Tabassum <120415170+arshutra@users.noreply.github.com>
2023-12-14 21:31:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+// ham
+
+function openmenu() {
+  document.getElementById('mobile-nav').classList.toggle('active')
+}
+
+function closemenu() {
+  document.getElementById('mobile-nav').classList.remove('active')
+}
+
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
+
+
+
+
